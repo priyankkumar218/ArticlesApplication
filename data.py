de4232b8f621092cbe55acab6f4fa6e8a3b188eb
priyankkumar218 <localhostservers@gmail.com>
2020-09-11 13:47:15 +0530

    first commit

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias amet culpa dicta dignissimos eum id impedit minima, necessitatibus nobis officiis pariatur, quidem quis reiciendis repellat sapiente sint temporibus vel!',
+            'author': 'Priyank Kumar',
+            'create_date': '04/25/2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias amet culpa dicta dignissimos eum id impedit minima, necessitatibus nobis officiis pariatur, quidem quis reiciendis repellat sapiente sint temporibus vel!',
+            'author': 'John Doe',
+            'create_date': '04/25/2017'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias amet culpa dicta dignissimos eum id impedit minima, necessitatibus nobis officiis pariatur, quidem quis reiciendis repellat sapiente sint temporibus vel!',
+            'author': 'Dummy User',
+            'create_date': '07/23/2019'
+        }
+    ]
+    return articles
